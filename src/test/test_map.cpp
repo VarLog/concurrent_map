70e6473bfdc24cb6903b0330916b500aa68c7369
Maxim Fedorenko (Macbook) <maxim.fedorenko@texturacorp.com>
2016-01-30 02:44:00 +0300

    A couple of new tests.

--- src/test/test_map.cpp
+++ src/test/test_map.cpp
@@ -134,3 +134,79 @@ TEST_F( MapTest, should_provide_exclusive_access_set ) {
     /// release token to avoid a deadlock into the destructor of the std::future from std::async()
     access_token.release();
 }
+
+TEST_F( MapTest, should_provide_exclusive_access_different_keys ) {
+    auto key1 = std::string( "key1" );
+    auto key2 = std::string( "key2" );
+    auto value1 = std::string( "value1" );
+    auto value2 = std::string( "value2" );
+
+    auto access_token = map_->get_exclusive_access( key1 );
+
+    auto fut = std::async( std::launch::async, [this, key2, value2] {
+        map_->set( key2, value2 );
+    } );
+
+    auto status = fut.wait_for( std::chrono::milliseconds( 500 ) );
+    EXPECT_TRUE( status == std::future_status::ready );
+
+    map_->set( key1, value1 );
+
+    auto actual1 = map_->get( key1 );
+
+    EXPECT_STREQ( value1.c_str(), actual1.c_str() );
+
+    access_token.release();
+
+    auto actual2 = map_->get( key2 );
+
+    EXPECT_STREQ( value2.c_str(), actual2.c_str() );
+}
+
+
+TEST_F( MapTest, should_provide_exclusive_access_many_keys ) {
+    auto key1 = std::string( "key1" );
+    auto key2 = std::string( "key2" );
+    auto value1 = std::string( "value1" );
+    auto value2 = std::string( "value2" );
+
+    auto access_token1 = map_->get_exclusive_access( key1 );
+    auto access_token2 = map_->get_exclusive_access( key2 );
+
+    auto fut = std::async( std::launch::async, [this, key1, key2, value2] {
+        map_->set( key1, value2 );
+        map_->set( key2, value2 );
+    } );
+
+    auto status = fut.wait_for( std::chrono::milliseconds( 500 ) );
+    EXPECT_TRUE( status == std::future_status::timeout );
+
+    map_->set( key1, value1 );
+    map_->set( key2, value1 );
+
+    auto actual1 = map_->get( key1 );
+
+    EXPECT_STREQ( value1.c_str(), actual1.c_str() );
+
+    auto actual2 = map_->get( key2 );
+
+    EXPECT_STREQ( value1.c_str(), actual2.c_str() );
+
+    access_token1.release();
+
+    status = fut.wait_for( std::chrono::milliseconds( 500 ) );
+    EXPECT_TRUE( status == std::future_status::timeout );
+
+    auto actual3 = map_->get( key1 );
+
+    EXPECT_STREQ( value2.c_str(), actual3.c_str() );
+
+    access_token2.release();
+
+    status = fut.wait_for( std::chrono::milliseconds( 500 ) );
+    EXPECT_TRUE( status == std::future_status::ready );
+
+    auto actual4 = map_->get( key2 );
+
+    EXPECT_STREQ( value2.c_str(), actual4.c_str() );
+}
